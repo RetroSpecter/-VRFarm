50d80b18440abf03a436542d7bfca81a44582e16
RetroSpecter <InsertNameHere4897@gmail.com>
2017-05-06 00:48:55 -0700

    firstPush
    VR farm demo

---
+++ VRFarmDemo2/Assets/CloudRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudRotation : MonoBehaviour {
+
+    public float speed;
+
+    void LateUpdate() {
+        transform.Rotate(0,speed,0);
+    }
+}
