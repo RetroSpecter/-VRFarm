eafaa529ee083db2420db5fb8548923aca359078
RetroSpecter <InsertNameHere4897@gmail.com>
2017-05-06 19:25:50 -0700

    grass sway
    added swaying ot the blades of grass

--- VRFarmDemo2/Assets/BillBoardGo.cs
+++ VRFarmDemo2/Assets/BillBoardGo.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 public class BillBoardGo : MonoBehaviour {
 
     public Transform MyCameraTransform;
-    private Transform MyTransform;
+    protected Transform MyTransform;
     public bool alignNotLook = true;
 
-    // Use this for initialization
-    void Start()
-    {
+    void Start() {
         MyTransform = this.transform;
         MyCameraTransform = Camera.main.transform;
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
+    void LateUpdate() {
         if (alignNotLook)
             MyTransform.forward = MyCameraTransform.forward;
         else
--- VRFarmDemo2/Assets/grassSkew.cs
+++ VRFarmDemo2/Assets/grassSkew.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class grassSkew : MonoBehaviour {
+public class grassSkew : BillBoardGo {
+    void Start() {
+        MyTransform = this.transform;
+        MyCameraTransform = Camera.main.transform;
+        randomStart = Random.Range(0,4);
 
+        if (alignNotLook)
+            MyTransform.forward = MyCameraTransform.forward;
+        else
+            MyTransform.LookAt(MyCameraTransform, Vector3.up);
+    }
 
-	
-	void Update () {
-        transform.Rotate(0,0,1);
-	}
+    float randomStart;
+    void LateUpdate() {
+        float rotate =  Mathf.Sin(randomStart + Time.time * 4) * 6 - 3;
+        print(rotate);
+
+        Quaternion lookAtCamera = Quaternion.LookRotation(MyCameraTransform.position);
+
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, rotate);
+    }
 }
